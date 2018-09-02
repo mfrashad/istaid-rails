8b1ac514847667a704824e551a8f066a8ad4852b
mfrashad <m.fathyrashad@gmail.com>
2018-09-02 22:23:05 +0800

    fix sorting on team view

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,5 +1,5 @@
 class EmployeesController < ApplicationController
   def index
-    @employees = Employee.all
+    @employees = Employee.order(:sort).all.paginate(page: params[:page], per_page: 20)
   end
 end
