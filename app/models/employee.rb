f1081c85c0b8b8412e709ac8813b4a3439cdda72
mfrashad <m.fathyrashad@gmail.com>
2018-09-03 11:41:32 +0800

    annotate employees

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -9,6 +9,7 @@
 #  created_at  :datetime         not null
 #  updated_at  :datetime         not null
 #  image       :string
+#  sort        :integer
 #
 
 class Employee < ActiveRecord::Base
--- test/models/employee_test.rb
+++ test/models/employee_test.rb
@@ -9,6 +9,7 @@
 #  created_at  :datetime         not null
 #  updated_at  :datetime         not null
 #  image       :string
+#  sort        :integer
 #
 
 require 'test_helper'
