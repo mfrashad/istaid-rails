5335c1f5853823b1cc85af45a25ff4de9b698bf4
mfrashad <m.fathyrashad@gmail.com>
2019-01-05 23:11:25 +0800

    Add show video

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -2,4 +2,8 @@ class VideosController < ApplicationController
   def index
     @videos = Video.all.paginate(page: params[:page], per_page: 20)
   end
+
+  def show
+    @video = Video.find(params[:id])
+  end
 end
