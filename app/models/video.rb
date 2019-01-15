d0ed6a9b0511321b9d86a48104a84e45c8a9e381
mfrashad <m.fathyrashad@gmail.com>
2019-01-15 20:19:04 +0800

    Add og tag to video

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,6 +1,19 @@
 class Video < ActiveRecord::Base
   validates :title, presence: true
 
+  def to_meta_tags
+    {
+      title: title,
+      image_src: thumbnail,
+      og: {
+        title: title,
+        type: 'video',
+        image: thumbnail,
+        url: youtube_url,
+      }
+    }
+  end
+
   def to_param
     slug
   end
