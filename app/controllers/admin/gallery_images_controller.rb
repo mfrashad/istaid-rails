53abdee29a4a6d5d248a2f608fee2c79c7e20a72
mfrashad <m.fathyrashad@gmail.com>
2019-01-15 20:46:36 +0800

    fix gallery images query by slug

--- app/controllers/admin/gallery_images_controller.rb
+++ app/controllers/admin/gallery_images_controller.rb
@@ -2,18 +2,18 @@ class Admin::GalleryImagesController < Admin::BaseController
   # truncated for brevity.
 
   def index
-    @gallery = Gallery.find(params[:gallery_id])
+    @gallery = Gallery.find_by_slug(params[:gallery_slug])
     @gallery_images = @gallery.gallery_images
   end
 
   def create
-    @gallery = Gallery.find(params[:gallery_id])
+    @gallery = Gallery.find_by_slug(params[:gallery_slug])
     @gallery_image = @gallery.gallery_images.create(gallery_image_params)
     @gallery_image.save
   end
 
   def destroy
-    @gallery = Gallery.find(params[:gallery_id])
+    @gallery = Gallery.find_by_slug(params[:gallery_slug])
     @gallery_image = GalleryImage.find(params[:id])
     @gallery_image.delete
     redirect_to admin_gallery_gallery_images_path(@gallery)
